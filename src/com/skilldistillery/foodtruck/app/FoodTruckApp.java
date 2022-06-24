631ea97dc74b3ef88c999e1153c8f99b694612c4
Jordy <castorena.jordy2@gmail.com>
2022-06-24 16:08:12 -0600

    initial commit

---
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -0,0 +1,15 @@
+package com.skilldistillery.foodtruck.app;
+
+import java.util.Scanner;
+
+import com.skilldistillery.foodtruck.entities.FoodTruck;
+
+public class FoodTruckApp {
+	private FoodTruck[] fleetOfFoodTrucks;
+//scanner - input from the user
+//ONLY static method 
+		public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -0,0 +1,5 @@
+package com.skilldistillery.foodtruck.entities;
+//create collection of FoodTrucks ( array - 5)
+public class FoodTruck {
+//unique numeric id, a name, food type, and a numeric rating
+}
