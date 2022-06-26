b497118f114effed6778c3c5b73a6e3e36bd9d01
Jordy <castorena.jordy2@gmail.com>
2022-06-25 22:06:30 -0600

    Finished application all user stories complete

--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -1,15 +1,113 @@
 package com.skilldistillery.foodtruck.app;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import com.skilldistillery.foodtruck.entities.FoodTruck;
 
 public class FoodTruckApp {
-	private FoodTruck[] fleetOfFoodTrucks;
+	private FoodTruck[] foodieFoodTrucks = new FoodTruck[5];
+
 //scanner - input from the user
 //ONLY static method 
-		public static void main(String[] args) {
+	public static void main(String[] args) {
+		Scanner userInput = new Scanner(System.in);
+		FoodTruckApp foodTruckApp = new FoodTruckApp();
+		foodTruckApp.startFoodTruckApp(userInput);
+
+	}
+
+	private void startFoodTruckApp(Scanner userInput) {
 		
+				for (int i = 0; i < foodieFoodTrucks.length; i++) {
+					try {
+					System.out.println("Please enter the Food Truck's name: ");
+					String name = userInput.nextLine();
+					if (name.equals("quit") || name.equals("QUIT") || name.equals("Quit")) {
+						break;
+					}
+					System.out.println("What is the food type? ");
+					String foodType = userInput.nextLine();
+					System.out.println("What is the rating? ");
+					double rating = userInput.nextDouble();
+					userInput.nextLine();
+					foodieFoodTrucks[i] = new FoodTruck(name, foodType, rating);
+					} catch (InputMismatchException e) {
+						userInput.nextLine();
+						i--;
+						System.out.println("Rating needs a numerical value. Item not saved. Please try again.");
+		 			}
+				}
+
+		boolean running = true;
+		do {
+			printMenu();
+			int menuChoice = userInput.nextInt();
+			switch (menuChoice) {
+			case 1:
+				listAllFoodTrucks(foodieFoodTrucks);
+				break;
+			case 2:
+				foodTruckAverage(foodieFoodTrucks);
+				break;
+			case 3:
+				highestRatedFoodTruck(foodieFoodTrucks);
+				break;
+			case 4:
+				System.out.println("Goodbye!");
+				running = false;
+				break;
+			default:
+				System.out.println("Please enter a valid number.");
+			}
+		} while (running);
+	}
+
+	private void printMenu() {
+		System.out.println("|\t      FoodieFoodTrucks       \t|");
+		System.out.println("|\t                             \t|");
+		System.out.println("|\t 1. List all Food Trucks     \t|");
+		System.out.println("|\t 2. Average ratings          \t|");
+		System.out.println("|\t 3. Highest rated food truck \t|");
+		System.out.println("|\t 4. Quit                     \t|");
+
+	}
+
+	private void listAllFoodTrucks(FoodTruck[] foodieFoodTrucks) {
+		for (int i = 0; i < foodieFoodTrucks.length; i++) {
+			if (foodieFoodTrucks[i] != null) {
+				System.out.println(foodieFoodTrucks[i].toString());
+			}
+		}
+	}
+
+	private void foodTruckAverage(FoodTruck[] foodieFoodTrucks) {
+		double sum = 0;
+		double average = 0;
+		int counter = 0;
+
+		for (int i = 0; i < foodieFoodTrucks.length; i++) {
+			if (foodieFoodTrucks[i] != null) {
+				sum = sum + foodieFoodTrucks[i].getRating();
+				counter++;
+			}
+		}
+		average = sum / counter;
+		System.out.println(average);
+	}
+
+	private void highestRatedFoodTruck(FoodTruck[] foodieFoodTrucks) {
+		double currentHighestRating = foodieFoodTrucks[0].getRating();
+		int highestRatingIndex = 0;
+		for (int i = 0; i < foodieFoodTrucks.length; i++) {
+			if (foodieFoodTrucks[i] != null && foodieFoodTrucks[i].getRating() > currentHighestRating) {
+				currentHighestRating = foodieFoodTrucks[i].getRating();
+				highestRatingIndex = i;
+
+			}
+		}
+		System.out.println(foodieFoodTrucks[highestRatingIndex].toString());
+
 	}
 
 }
--- src/com/skilldistillery/foodtruck/entities/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -1,5 +1,84 @@
 package com.skilldistillery.foodtruck.entities;
-//create collection of FoodTrucks ( array - 5)
+
+import java.util.Objects;
+
+//create collection of FoodTrucks ( array / 5)
 public class FoodTruck {
 //unique numeric id, a name, food type, and a numeric rating
+	private int id;
+	static int foodTruckId = 1;
+	private String name;
+	private String foodType;
+	private double rating;
+	
+	public FoodTruck() {
+	}
+
+	public FoodTruck(String name, String foodType, double rating) {
+		super();
+		this.name = name;
+		this.foodType = foodType;
+		this.rating = rating;
+		this.id = foodTruckId;
+		foodTruckId++;
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+
+	public double getRating() {
+		return rating;
+	}
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(foodType, id, name, rating);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FoodTruck other = (FoodTruck) obj;
+		return Objects.equals(foodType, other.foodType) && id == other.id && Objects.equals(name, other.name)
+				&& Double.doubleToLongBits(rating) == Double.doubleToLongBits(other.rating);
+	}
+
+	@Override
+	public String toString() {
+		return "FoodTruck [id=" + id + ", name=" + name + ", foodType=" + foodType + ", rating=" + rating + "]";
+	}
+	
+	
+	
 }
